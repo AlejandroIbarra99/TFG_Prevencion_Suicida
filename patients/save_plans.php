eeb1897dce9ab90e4301df0e504c67d960f0bc32
Alejandro Ibarra del Estal <alexibarragohs@gmail.com>
2023-04-28 12:03:35 +0200

    Se actualiza el código funcionando el alta y actualizacion de los planes futuros

--- patients/save_plans.php
+++ patients/save_plans.php
@@ -1,22 +1,15 @@
 <?php
 session_start();
-// Conexión a la base de datos
+// Conectamos con la base de datos
 $servername = "localhost";
 $username = "sa";
 $password = "1234";
 $dbname = "contigo";
-$id = 0;
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 // Comprobamos la conexión
 if ($conn->connect_error) {
   die("Conexión fallida: " . $conn->connect_error);
 }
-
-if (isset($_SESSION['patient_id'])) {
-    $id = $_SESSION['patient_id'];
-}
-
 // Obtener datos del formulario enviado desde el cliente
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $plans = isset($_POST['plan']) ? $_POST['plan'] : array();
@@ -26,29 +19,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     foreach ($plans as $key => $plan) {
         $plan = trim($plan);
         // Comprobar si el plan ya existe en la base de datos
-        $stmt = $conn->prepare("SELECT id FROM plans WHERE plans_definition = ? AND patient_id = ? AND plans_done = 0");
-        $stmt->bind_param("si", $plan, $id);
+        $stmt = $conn->prepare("SELECT id FROM plans WHERE plans_definition = ? AND patient_id = ? ");
+        $stmt->bind_param("si", $plan, $_SESSION['patient_id']);
         $stmt->execute();
         $stmt->store_result();
 
         if ($stmt->num_rows == 0) {
             // El plan no existe en la base de datos, guardarlo
+            //$is_done = isset($done[$key]) && $done[$key] ? 1 : 0;
             $is_done = isset($is_done[$key]) && $is_done[$key] == "1" ? 1 : 0;
             $data[] = array('plan' => $plan, 'is_done' => $is_done);
         } else {
-            // El plan ya existe en la base de datos, actualizar su estado de realización
-            foreach ($_POST as $key => $value) {
-              if (strpos($key, "done") === 0) {
-                  $plan_id = substr($key, 4); // obtener el ID del plan desde el nombre de la casilla
-                  $plan_done = intval($value); // convertir el valor del checkbox a un entero (0 o 1)
-                  
-                  // actualizar la base de datos
-                  $sql = "UPDATE plans SET plans_done = 1 WHERE id = "  . $plan_done;
-                  if ($conn->query($sql) === false) {
-                      echo "Error actualizando el plan con ID " . $plan_id . ": " . $conn->error;
-                  }
+               // El plan ya existe en la base de datos, actualizar su estado de realización
+               foreach ($_POST as $key => $value) {
+                if (strpos($key, "done") === 0) {
+                    $plan_id = substr($key, 4); // obtener el ID del plan desde el nombre de la casilla
+                    $plan_done = intval($value); // convertir el valor del checkbox a un entero (0 o 1)
+                    
+                    // actualizar la base de datos
+                    $sql = "UPDATE plans SET plans_done = 1 WHERE id = "  . $plan_done;
+                    if ($conn->query($sql) === false) {
+                        echo "Error actualizando el plan con ID " . $plan_id . ": " . $conn->error;
+                    }
+                }
               }
-            }
+          }
         }
         $stmt->close();
         $i++; // incrementar la variable $i en cada iteración
@@ -57,11 +52,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (!empty($data)) {
         // Preparar consulta preparada y vincular los parámetros
         $stmt = $conn->prepare("INSERT INTO plans (plans_definition, plans_done, patient_id) VALUES (?, ?, ?)");
-        $stmt->bind_param("sii", $plan, $is_done, $id);
+        $stmt->bind_param("sii", $plan, $is_done, $_SESSION['patient_id']);
         foreach ($data as $plan) {
             $plan = isset($plan['plan']) ? $plan['plan'] : "";
             $is_done = isset($plan['is_done']) && $plan['is_done'] ? 1 : 0;
-            $stmt->bind_param("sii", $plan, $is_done, $id);
+            $stmt->bind_param("sii", $plan, $is_done, $_SESSION['patient_id']);
             $stmt->execute();
         }
         $stmt->close();
@@ -78,8 +73,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
               }, 2000);
             </script>";
     }
-}
-
 // Cerrar la conexión
 $conn->close();
 ?>

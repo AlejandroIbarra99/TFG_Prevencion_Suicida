d1861684746008e1cb033c831e305224bbed00e0
Alejandro Ibarra del Estal <alexibarragohs@gmail.com>
2023-05-16 10:17:53 +0200

    Se muestra las entradas al diario desde el entorno del psicologo

--- psychos/get_safe_zone_info.php
+++ psychos/get_safe_zone_info.php
@@ -22,11 +22,18 @@ if (isset($_GET['id'])) {
   $result2 = $conn->query($sql2);
   $sql3 = "SELECT * FROM photos WHERE patient_id = '$id'";
   $result3 = $conn->query($sql3);
+  $sql4 = "SELECT DATE_FORMAT(daily_date, '%d/%m/%Y') AS formatted_date, GROUP_CONCAT(daily_entry SEPARATOR ' ') AS entries 
+  FROM dailys 
+  WHERE patient_id = '$id' 
+  GROUP BY DATE_FORMAT(daily_date, '%d/%m/%Y') 
+  ORDER BY daily_date DESC";
+  $result4 = $conn->query($sql4);
   echo "<table class='table table-striped table-hover'> <thead>";
   if ($result->num_rows > 0) {
     // Muestra la información de la 'safe zone' del paciente seleccionado en una tabla HTML
-    
-            echo "<tr>
+    echo "<table class='table table-striped table-hover'>
+            <thead>
+              <tr>
                 <th scope='col'>Contacto de emergencia</th>
                 <th scope='col'>Teléfono de contacto emergencia</th>
                 <th scope='col'>Planes de futuro</th>
@@ -35,24 +42,27 @@ if (isset($_GET['id'])) {
             </thead>
             <tbody>";
 
-    while ($row = $result->fetch_assoc()) {
-      echo "<tr>
-              <td>{$row['contact_name']}</td>
-              <td>{$row['contact_phone']}</td>
-    }";
-    while ($row2 = $result2->fetch_assoc()) {
-              "<td>{$row2['plans_definition']}</td>";
-    }
-    while ($row3 = $result3->fetch_assoc()) {
-              "<td>{$row3['daily_entry']}</td>
-              <!-- Agrega aquí más celdas del 'Espacio Seguro' según sea necesario -->
-            </tr>";
-    }
-
-    echo "  </tbody>
-          </table>";
-  }
-  } else {
+            while ($row = $result->fetch_assoc()) {
+              echo "<tr>
+                      <td>{$row['contact_name']}</td>
+                      <td>{$row['contact_phone']}</td>";
+        
+              while ($row2 = $result2->fetch_assoc()) {
+                echo "<td>{$row2['plans_definition']}</td>";
+              }
+        
+              echo "<td>";
+              while ($row4 = $result4->fetch_assoc()) {
+                echo "{$row4['formatted_date']}<br>{$row4['entries']}<br>";
+              }
+              echo "</td>";
+        
+              echo "</tr>";
+            }
+        
+            echo "</tbody>
+                  </table>";
+          } else {
     echo "No se encontró información del 'Espacio Seguro' para el paciente seleccionado.";
   }
 }
